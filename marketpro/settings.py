c035961fe09d5769e719acdb2153c12c96129d02
Sajith Tomas <sajiththomas231@gmail.com>
2024-09-24 19:16:44 +0530

    hyyy

--- marketpro/settings.py
+++ marketpro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b5#1q6r01z9b2@^!3oyksd#+qw2nr9fn_98p+n3h@51r_4t1m_
 DEBUG = True
 
 # ALLOWED_HOSTS = ['super-market-h0gf.onrender.com','localhost']
-ALLOWED_HOSTS = ['super-market-h0gf.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['super-market-h0gf.onrender.com']
 
 
 
