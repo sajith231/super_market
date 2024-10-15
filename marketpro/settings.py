3b07ffb5ba8e57be8f4932f170b747c773865ed8
Sajith Tomas <sajiththomas231@gmail.com>
2024-10-15 10:29:14 +0530

    hyyy

--- marketpro/settings.py
+++ marketpro/settings.py
@@ -157,9 +157,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR/"static"
-    ]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
